2a2909bdfa0cb1d5ab84983450ddf246551e0ca1
stivex001 <stephenadeyemo@gmail.com>
2023-04-28 15:37:58 +0100

    feat:implementing the menu bar components

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,12 +1,38 @@
-import styled from "styled-components";
+import styled from "styled-components"
+import youtube from "../../assets/Group.png"
 
 const Container = styled.div`
 flex: 1;
+background-color: #212121;
+height: 100vh;
+color: white;
+font-size: 14px;
 `;
+const Wrapper = styled.div`
+padding: 18px 26px;
+`;
+const Logo = styled.div`
+display: flex;
+align-items: center;
+gap: 5px;
+font-weight: bold;
+margin-bottom: 25px;
+`;
+const Img = styled.img`
+height: 25px;
+`;
+
 type Props = {};
 
 const Menu = (props: Props) => {
-  return <Container>Menu</Container>;
+  return <Container>
+    <Wrapper>
+        <Logo>
+            <Img src={youtube} />
+            BaiTube
+        </Logo>
+    </Wrapper>
+  </Container>;
 };
 
 export default Menu;
