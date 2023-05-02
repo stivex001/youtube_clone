f0da11808477204ba46dc59435ec08039f29454a
stivex001 <stephenadeyemo@gmail.com>
2023-05-02 14:30:23 +0100

    feat: added stylings to the navbar components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,36 +3,39 @@ import ActionButton from "../shared/ActionButton";
 import { SearchOutlined } from "@mui/icons-material";
 
 const Container = styled.div`
-position: sticky;
-top: 0;
-background-color: ${({theme}) => theme.bgLighter};
-height: 56px;
+  position: sticky;
+  top: 0;
+  background-color: ${({ theme }) => theme.bgLighter};
+  height: 56px;
 `;
 const Wrapper = styled.div`
-display: flex;
-align-items: center;
-justify-content: flex-end;
-height: 100%;
-padding: 0px 20px;
-position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 100%;
+  padding: 0px 20px;
+  position: relative;
 `;
 const Search = styled.div`
-width: 40%;
-display: flex;
-align-items: center;
-justify-content: space-between;
-position: absolute;
-left: 0;
-right: 0;
-margin: 0 auto;
-padding: 5px;
-border: 1px solid #cccccc;
+  width: 40%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  padding: 5px;
+  border: 1px solid #cccccc;
+  border-radius: 3px;
 `;
 const Input = styled.input`
-
+  border: none;
+  background-color: transparent;
+  outline: none;
 `;
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
@@ -42,12 +45,10 @@ const Navbar = (props: Props) => {
           <Input placeholder="Search" />
           <SearchOutlined />
         </Search>
-        <ActionButton>
-          SIGN IN
-        </ActionButton>
+        <ActionButton>SIGN IN</ActionButton>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
