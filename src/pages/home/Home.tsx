9408da57869e1b6a85a827fd89e63e41e32f50fc
stivex001 <stephenadeyemo@gmail.com>
2023-05-02 23:01:27 +0100

    fix: code refactored

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
-
-type Props = {}
+type Props = {};
 
 const Home = (props: Props) => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <div>Home</div>;
+};
 
-export default Home
+export default Home;
