4e8d1ec6575e7eb300dac4233c32464e3fa139ff
stivex001 <stephenadeyemo@gmail.com>
2023-05-02 22:49:28 +0100

    feat:added react router package to the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,34 @@ import Menu from "./components/menu/Menu";
 import Navbar from "./components/Navbar/Navbar";
 import { darkTheme, lightTheme } from "./utils/Theme";
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 const Container = styled.div`
   display: flex;
 `;
 const Main = styled.div`
   flex: 7;
-  background-color:  ${({ theme }) => theme.bgLighter};
+  background-color: ${({ theme }) => theme.bgLighter};
 `;
 const Wrapper = styled.div``;
 
 const App: React.FC = () => {
   const [darkMode, setDarkMode] = useState<boolean>(true);
   return (
-    <ThemeProvider theme={darkMode? darkTheme : lightTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
-        <Menu setDarkMode={setDarkMode} darkMode={darkMode}/>
-
-        <Main>
-          <Navbar />
-          <Wrapper>videos</Wrapper>
-        </Main>
+        <BrowserRouter>
+          <Menu setDarkMode={setDarkMode} darkMode={darkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
       </Container>
     </ThemeProvider>
   );
