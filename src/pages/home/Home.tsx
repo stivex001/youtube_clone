d37199cabf9375075209da16854a203b1b7de171
stivex001 <stephenadeyemo@gmail.com>
2023-05-03 07:13:55 +0100

    feat: implemented card comonent

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,22 @@
+import styled from "styled-components";
+import Card from "../../components/shared/Card";
+
+const Container = styled.div``;
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  return <Container>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </Container>;
 };
 
 export default Home;
